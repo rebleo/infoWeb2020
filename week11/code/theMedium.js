c1c41eaea821e834c62f037b6d7fa8abf42d4e14
rebleo <rml444@nyu.edu>
2020-11-02 16:18:59 -0500

    init js

---
+++ week11/code/theMedium.js
@@ -0,0 +1,13 @@
+console.log("js!");
+
+let theButton = document.querySelector("button");
+
+
+// triggering an event with a callback function
+//
+// theButton.addEventListener('click', theClick);
+
+// function theClick(){
+//  console.log("cick!!!");
+ 
+// }
